c63e2318c19996cc7ce3a815088ca6bf0cbef941
Natalia Bezerra <b1z3rr4@gmail.com>
2024-10-09 10:25:08 -0300

    logo

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Button } from "../../components/elements/Button";
 import { useHistory } from "../../../application/libs/history";
+import { Logo, Logo2, Logo3 } from "../../components/modules/Logo/Logo";
 
 export function Home() {
   const { navigate } = useHistory();
 
   return (
     <div>
+      <Logo />
+      <Logo2 />
+      <Logo3 />
       <h1>Bem vindo(a) a Home!</h1>
       <Button
         label="Ir para form"
