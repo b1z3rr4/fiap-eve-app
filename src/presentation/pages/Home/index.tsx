626d9f8e79fbb7122c95798e9823e64129138ace
Jessica <jessicaoliveira.dev1@gmail.com>
2024-10-11 14:54:28 -0300

    wip

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Card } from "../../components/modules/Card";
+import { IEvent } from "../../../application/models/event";
 
 export function Home() {
+  const [events, setEvents] = useState<IEvent[]>([]);
+
+  useEffect(() => {
+    console.log('veio aqui');
+    const mockEvents: IEvent[] = [
+      {
+        day: "2024-10-15",
+        name: "Encontro virtual Literatura e Criatividade",
+        hour: "14:00",
+        type: "Virtual",
+        photo: "https://via.placeholder.com/150",
+        price: 0,
+        creator: "João",
+        latitude: "-23.550520",
+        longitude: "-46.633308",
+        vacancies: 20,
+        description: "Vamos fazer um encontro virtual...",
+        manyRequests: false,
+      },
+      {
+        day: "2024-10-18",
+        name: "Workshop de Tecnologia",
+        hour: "10:00",
+        type: "Presencial",
+        photo: "https://via.placeholder.com/150",
+        price: 50,
+        creator: "Maria",
+        latitude: "-23.550520",
+        longitude: "-46.633308",
+        vacancies: 15,
+        description: "Discussão sobre as tendências de tecnologia...",
+        manyRequests: true,
+      },
+    ];
+    setEvents(mockEvents);
+  }, []);
 
   return (
     <div>
-      <Card></Card>
+      {events.map((e) => (
+        <Card key={e.name} event={e}/>
+      ))}
       <Outlet />
     </div>
   );
