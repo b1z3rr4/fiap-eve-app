dde070164b00029f1b70fdee30395bcafc0036ac
Jessica <jessicaoliveira.dev1@gmail.com>
2024-10-11 15:23:33 -0300

    wip

--- src/presentation/components/modules/Card/index.tsx
+++ src/presentation/components/modules/Card/index.tsx
@@ -1,4 +1,6 @@
+import { useNavigate } from "react-router-dom";
 import { IEvent } from "../../../../application/models/event";
+import { Button } from "../../elements/Button";
 import * as S from "./styles";
 
 interface CardProps {
@@ -6,11 +8,14 @@ interface CardProps {
 }
 
 export const Card = ({ event }: CardProps) => {
+    const navigate = useNavigate();
+
     return(
         <S.CardContainer>
             <img src={event.photo} alt={event.name}/>
             <S.CardTitle>{event.name}</S.CardTitle>
             <S.CardDescription>{event.description}</S.CardDescription>
+            <Button onClick={() => navigate(`/edit/1`)}>Editar</Button>
         </S.CardContainer>
     )
 };
