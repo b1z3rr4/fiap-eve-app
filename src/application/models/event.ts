69f7d3996f09d66ad0bb00588081f7371fca17a3
Jessica <jessicaoliveira.dev1@gmail.com>
2024-10-11 14:22:29 -0300

    wip

---
+++ src/application/models/event.ts
@@ -0,0 +1,14 @@
+export interface IEvent {
+    day: string;
+    name: string;
+    hour: string;
+    type: string;
+    photo: string;
+    price: number;
+    creator: string;
+    latitude: string;
+    longitude: string;
+    vacancies: number;
+    description: string;
+    manyRequests: boolean;
+};
